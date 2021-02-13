50de20f3d90b3cd6367b58d303f5f6c21a0b9a23
Ross Bryan <bryanro@med.umich.edu>
2021-02-13 18:43:02 -0500

    [IMP] lots of changes since i've last commited anything. should be better about that

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+gcloud config set project deft-computing-303901
 docker build -t us-central1-docker.pkg.dev/deft-computing-303901/rtbryan/mysite:1.0.0 . 
 docker push us-central1-docker.pkg.dev/deft-computing-303901/rtbryan/mysite:1.0.0
 gcloud run deploy mysite --region=us-central1 --image us-central1-docker.pkg.dev/deft-computing-303901/rtbryan/mysite:1.0.0 --platform managed
